5dfb917fb8d6db0631300cd7fa93b9b76679ce91
Alena Tiameikina <aaldoshina@ostrovok.ru>
2024-05-07 19:38:17 +0300

    change for conflict

--- tests_features/steps_data.py
+++ tests_features/steps_data.py
@@ -29,7 +29,7 @@ distance_4 = {'distance': 30,
               'workload': 'high'}
 
 small = {'distance': 10,
-         'dimension': 'small one',
+         'dimension': 'one small',
          'is_fragile': False,
          'workload': 'high one'}
 
